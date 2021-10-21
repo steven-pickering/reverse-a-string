e0435251d2b2b9f655a1afcb607a22e4a84f46fc
Steven Pickering <52013044+Steven909@users.noreply.github.com>
2021-10-22 00:24:27 +0100

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,19 @@
+// Enter a string and the program will reverse it and print it out.
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        reverseInputString();
+
+
+    }
+
+    private static void reverseInputString() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+
+    }
+}
