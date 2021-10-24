110156ff9a3f64297392003aeca95c82a8fd37f9
Steven Pickering <52013044+Steven909@users.noreply.github.com>
2021-10-24 18:00:47 +0100

    Working commit

--- src/App.java
+++ src/App.java
@@ -1,21 +1,28 @@
-// Enter a string and the program will reverse it and print it out.
-
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
         reverseInputtedString();
-
-
     }
 
-    // Takes a string input from user, reverses the input and prints it back out
+    // takes a string input from user, reverses the input and prints it back out
     private static void reverseInputtedString() {
+        System.out.println("Enter a string > ");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
 
+        StringBuilder input1 = new StringBuilder();
+
+        // append a string into StringBuilder input1
+        input1.append(input);
+
+        // reverse StringBuilder input1
+        input1 = input1.reverse();
+
+        // print reversed String
+        System.out.println(input1);
 
 
     }
